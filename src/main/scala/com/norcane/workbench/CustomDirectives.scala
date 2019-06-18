bcdd194092d0aff648f9a00a9a2a91cba6a3b606
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-06-18 12:31:15 +0200

    Minor cleanup

--- src/main/scala/com/norcane/workbench/CustomDirectives.scala
+++ src/main/scala/com/norcane/workbench/CustomDirectives.scala
@@ -14,47 +14,24 @@ import akka.http.scaladsl.server.directives.RouteDirectives.reject
 import scala.annotation.tailrec
 
 /**
- **
- Akka was made more secure via
-  *https://github.com/akka/akka-http/issues/365
-  *https://github.com/akka/akka-http/issues/365
- **
- Which made it no longer follow symlinks, which for a developer server is a bit too secure.  So this copies blatantly from akka http's
-  *https://github.com/akka/akka-http/blob/master/akka-http/src/main/scala/akka/http/scaladsl/server/directives/FileAndResourceDirectives.scala
- **
- Reason being there are private methods in there and no real way to override.  The re-enabling of symlinks is in the commented out code in def checkIsSafeDescendant
-  *
+  * Akka HTTP no longer follow symlinks (see https://github.com/akka/akka-http/issues/365), which is unsuitable for a
+  * developer server. Following code is mainly copied from
+  * https://github.com/akka/akka-http/blob/master/akka-http/src/main/scala/akka/http/scaladsl/server/directives/FileAndResourceDirectives.scala
+  * as there's no proper way how to override private methods.
   */
 object CustomDirectives {
 
   private val followSymlinks = true
 
-  /**
-    * Same as `getFromBrowseableDirectories` with only one directory.
-    *
-    * @group fileandresource
-    */
   def getFromBrowseableDirectory(directory: String)(implicit renderer: DirectoryRenderer,
                                                     resolver: ContentTypeResolver): Route =
     getFromBrowseableDirectories(directory)
 
-  /**
-    * Serves the content of the given directories as a file system browser, i.e. files are sent and directories
-    * served as browseable listings.
-    *
-    * @group fileandresource
-    */
   def getFromBrowseableDirectories(directories: String*)(implicit renderer: DirectoryRenderer,
                                                          resolver: ContentTypeResolver): Route = {
     directories.map(getFromDirectory).reduceLeft(_ ~ _) ~ listDirectoryContents(directories: _*)
   }
 
-  /**
-    * Completes GET requests with the content of a file underneath the given directory.
-    * If the file cannot be read the Route rejects the request.
-    *
-    * @group fileandresource
-    */
   def getFromDirectory(directoryName: String)(implicit resolver: ContentTypeResolver): Route =
     extractUnmatchedPath { unmatchedPath ⇒
       extractLog { log ⇒
@@ -71,7 +48,7 @@ object CustomDirectives {
                                      separator: Char = File.separatorChar): String =
     safeJoinPaths(basePath, path, log, separator) match {
       case "" ⇒ ""
-      case path ⇒ checkIsSafeDescendant(basePath, path, log)
+      case p ⇒ checkIsSafeDescendant(basePath, p, log)
     }
 
   private def withTrailingSlash(path: String): String = if (path endsWith "/") path else path + '/'
